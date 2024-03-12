68514497727085917e7497f7fd956b10f150c1ef
Alexandre EDMOND <alexandre.edmond31@gmail.com>
2024-03-12 21:20:25 +0100

    Flappy Bird avec Pygame totalement fonctionnel et modulable

--- flappy.py
+++ flappy.py
@@ -1,4 +1,4 @@
-
+import pygame
 
 class Flappy:
 
@@ -10,6 +10,9 @@ class Flappy:
         self.speed_decrease = 0.001
         self.speed_jump = 0.5
     
+    def get_rect(self):
+        return pygame.Rect(self.x, self.y, self.width, self.width)
+
     def actualise_position(self):
         self.y -= self.speed
 
--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 from flappy import *
 from obstacles import *
+from random import randint
 
 class Game:
 
@@ -10,32 +11,42 @@ class Game:
         self.screen = pygame.display.set_mode((self.w, self.h))
         self.flappy = Flappy(150, 300)
         self.list_obstacles = []
-        self.next_obstacle = 1000
-        self.time_between_obstacle = 3000
+        self.frame = 0
+        self.next_obstacle = 3000
+        self.time_between_obstacle = 5000
+        self.width_wall = 30
+        self.width_center = 100
     
     def add_obstacle(self):
-        self.list_obstacles.append(Obstacle(self.w, 300))
+        self.list_obstacles.append(Obstacle(self.w, randint(self.width_center + 10, self.h - self.width_center + 10), self.h, self.width_wall, self.width_center))
         self.next_obstacle += self.time_between_obstacle
 
     def delete_obstacle(self):
         for obstacle in self.list_obstacles:
             if obstacle.x <= 0:
                 self.list_obstacles.remove(obstacle)
-                print(len(self.list_obstacles))
         
 
     def colision_elements(self):
         # Détection si le flappy touche le sol ou le haut
         if self.flappy.y <= 0 or self.flappy.y >= self.h - self.flappy.width:
             return 1
+        
+        # Détection de colision entre floppy et les obstacles
+        for obstacle in self.list_obstacles:
+            if pygame.Rect.colliderect(self.flappy.get_rect(), obstacle.get_rect_top()) == True:
+                return 1
+            if pygame.Rect.colliderect(self.flappy.get_rect(), obstacle.get_rect_down()) == True:
+                return 1
+            
         return 0
 
     def affichage_elements(self):
-        pygame.draw.rect(self.screen, (200, 0, 0), pygame.Rect(self.flappy.x, self.flappy.y, self.flappy.width, self.flappy.width))
+        pygame.draw.rect(self.screen, (200, 0, 0), self.flappy.get_rect())
 
         for obstacle in self.list_obstacles:
-            pygame.draw.rect(self.screen, (0, 200, 0), pygame.Rect(obstacle.x - obstacle.width, 0, obstacle.width, obstacle.y - obstacle.width_center))
-            pygame.draw.rect(self.screen, (0, 200, 0), pygame.Rect(obstacle.x - obstacle.width, obstacle.y + obstacle.width_center, obstacle.width, obstacle.y - obstacle.width_center))
+            pygame.draw.rect(self.screen, (0, 200, 0), obstacle.get_rect_top())
+            pygame.draw.rect(self.screen, (0, 200, 0), obstacle.get_rect_down())
 
     def actualise_elements(self):
         self.flappy.decrease_speed()
@@ -44,7 +55,7 @@ class Game:
         for obstacle in self.list_obstacles:
             obstacle.actualise_position()
 
-        if pygame.time.get_ticks() > self.next_obstacle:
+        if self.frame > self.next_obstacle:
             self.add_obstacle()
 
     def main_loop(self):
@@ -71,3 +82,4 @@ class Game:
             self.affichage_elements()
 
             pygame.display.flip()
+            self.frame += 1
